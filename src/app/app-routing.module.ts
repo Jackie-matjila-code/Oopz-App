484af27954982f80d6cc8e6a34cf6f79332fb174
matjilatj2@gmail.com <matjilatj2@gmail.com>
2021-04-09 03:47:18 +0200

    added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'dash-board',
+    loadChildren: () => import('./Client/dash-board/dash-board.module').then( m => m.DashBoardPageModule)
+  },
+   {
+      path: '',
+     redirectTo: 'dash-board',
+     pathMatch: 'full'
+   },
+  {
+    path: 'register',
+    loadChildren: () => import('./Client/register/register.module').then( m => m.RegisterPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./Client/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./Client/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./Admin/login/login.module').then( m => m.LoginPageModule)
+  },
+
+
 ];
 
 @NgModule({
